9ca8013101c2484c34f00ce26259e338a838d046
Andreas Pichler <andreas.pichler.it@hotmail.com>
2017-10-25 09:35:16 +0200

    test

---
+++ TextChecker/ViewModels/PalindromeViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System.Windows;
+using Caliburn.Micro;
+
+namespace TextChecker.ViewModels
+{
+    public class PalindromeViewModel : Screen
+    {
+        private string _text;
+        public void HaveResult()
+        {
+            string reversedText = "";
+
+            for( int i = text.Length - 1; i >= 0; --i )
+            {
+                reversedText = reversedText + text[i];
+            }
+
+            if( text.ToLower().Replace( " ", "" ).Replace( ".", "" ).Replace( ",", "" ).Replace( ";", "" ) == reversedText.ToLower().Replace( " ", "" ).Replace( ".", "" ).Replace( ",", "" ).Replace( ";", "" ) )
+            {
+                MessageBox.Show( "Ihre Eingabe ist ein Palindrom" + "\n\nGRÜN" );
+            }
+            else
+            {
+                MessageBox.Show( "Ihre Eingabe ist kein Palindrom" + "\n\nROT" );
+            }
+        }
+        public string text
+        {
+            get { return _text; }
+            set
+            {
+                _text = value;
+                NotifyOfPropertyChange( () => text );
+                NotifyOfPropertyChange( () => CanHaveResult );
+            }
+        }
+        public bool CanHaveResult
+        {
+            get { return !string.IsNullOrWhiteSpace( text ); }
+        }
+    }
+}
---
+++ TextChecker/ViewModels/ShellViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Caliburn.Micro;
+using TextChecker.Models;
+
+namespace TextChecker.ViewModels
+{
+    public class ShellViewModel : PropertyChangedBase, 
+    {
+        public void LoadPalindrome()
+        {
+            ActivateItem( new PalindromeViewModel() );
+        }
+        public void LoadEvenOrOdd()
+        {
+            ActivateItem( new EvenOrOddViewModel() );
+        }
+        public void LoadPageOne()
+        {
+            ActivateItem( new FirstChildViewModel() );
+        }
+        public void LoadPageTwo()
+        {
+            ActivateItem( new SecondChildViewModel() );
+        }
+    }
+}
